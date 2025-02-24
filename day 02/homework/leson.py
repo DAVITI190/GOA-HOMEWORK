adb2be6d282e9e1813bbd6a2bfb14b32e9c0cda7
DAVITI190 <tugushidaviti86@gmail.com>
2025-02-24 13:14:48 +0400

    commit

---
+++ day 02/homework/leson.py
@@ -0,0 +1,6 @@
+
+str_example = "Hello"
+
+int_example = 100
+print(str_example)  
+print(int_example)
---
+++ day 02/homework/leson1.py
@@ -0,0 +1,16 @@
+
+str1 = "გამარჯობა"
+str2 = "ელემენტი " * 10  
+
+int1 = 500
+int2 = 500
+int3 = int1 + int2  
+
+str3 = "hello" + " world"  
+bool_example = (10 * 2) == 20  
+
+print(str1)  
+print(str2)  
+print(int3)
+print(str3)  
+print(bool_example)  
